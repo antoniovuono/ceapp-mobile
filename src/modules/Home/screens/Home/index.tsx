8cbabb8bb3cb8b54b72991dab1e68d01af22d477
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-11-21 12:56:35 -0300

    eslint configured and project sctruture starts

---
+++ src/modules/Home/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, WelcomeTitle } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <WelcomeTitle>Welcome to ceaap mobile</WelcomeTitle>
+    </Container>
+  );
+};
+
+export default Home;
