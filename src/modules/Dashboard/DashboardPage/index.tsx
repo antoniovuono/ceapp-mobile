20cec9ae6b055ac02a2797ecc8557ba60f0be951
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-16 20:25:50 -0300

    We resolve the click on chart!

--- src/modules/Dashboard/DashboardPage/index.tsx
+++ src/modules/Dashboard/DashboardPage/index.tsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTheme } from 'styled-components';
+import { Bar, VictoryBar, VictoryChart } from 'victory-native';
 import Header from '../../../components/Header';
-import { Container, Title } from './styles';
+import { Container } from './styles';
+
+const data = [
+  { x: 'Jan', y: 2 },
+  { x: 'Fev', y: 3 },
+  { x: 'Mar', y: 5 },
+  { x: 'Abr', y: 4 },
+  { x: 'Mai', y: 10 },
+  { x: 'Jun', y: 4 },
+  { x: 'Jul', y: 4 },
+  { x: 'Agos', y: 4 },
+  { x: 'Set', y: 4 },
+  { x: 'Out', y: 4 },
+  { x: 'Nov', y: 4 },
+  { x: 'Dez', y: 4 },
+];
 
 const DashboardPage: React.FC = () => {
+  const theme = useTheme();
+
   return (
-    <Container>
+    <>
       <Header />
-      <Title>Dashboard</Title>
-    </Container>
+      <Container>
+        <VictoryChart
+          height={400}
+          width={430}
+          domainPadding={{ x: 13, y: [0, 40] }}
+        >
+          <VictoryBar
+            style={{
+              data: {
+                fill: theme.colors.SUCCESS_GREEN,
+                width: 18,
+              },
+              labels: {},
+            }}
+            data={data}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onPressIn: () => {
+                    return [
+                      {
+                        target: 'data',
+                        mutation: () => ({
+                          style: {
+                            fill: theme.colors.SECONDARY_WARNING_YELLOW,
+                            width: 18,
+                          },
+                        }),
+                      },
+                    ];
+                  },
+                },
+              },
+            ]}
+          />
+        </VictoryChart>
+      </Container>
+    </>
   );
 };
 
--- src/modules/Dashboard/DashboardPage/styles.ts
+++ src/modules/Dashboard/DashboardPage/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
 `;
-
-export const Title = styled.Text``;
